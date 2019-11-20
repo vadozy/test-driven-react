2e483461e7dd758621f2bbe1eeb71cece7c59539
Vadim Storozhuk <vadozy@yahoo.com>
2019-11-19 20:48:53 -0500

    :ok_hand: End of Ch04

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -23,10 +23,14 @@ class Carousel extends React.PureComponent {
   };
 
   render() {
-    const { slides, ...rest } = this.props;
+    const { defaultImg, defaultImgHeight, slides, ...rest } = this.props;
     return (
       <div {...rest}>
-        <CarouselSlide {...slides[this.state.slideIndex]} />
+        <CarouselSlide
+          Img={defaultImg}
+          imgHeight={defaultImgHeight}
+          {...slides[this.state.slideIndex]}
+        />
         <CarouselButton data-action="prev" onClick={this.handlePrevClick}>
           Prev
         </CarouselButton>
@@ -38,8 +42,15 @@ class Carousel extends React.PureComponent {
   }
 
   static propTypes = {
+    defaultImg: CarouselSlide.propTypes.Img,
+    defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
       .isRequired,
+  };
+
+  static defaultProps = {
+    defaultImg: CarouselSlide.defaultProps.Img,
+    defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   };
 }
 
